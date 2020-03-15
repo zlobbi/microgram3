333431d1c093c6a94531214e29dd977d5cdd6199
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-03-15 21:42:28 +0600

    added ftlh pages, controller & repo for subscriptions

---
+++ src/main/java/km/hw52/microgram/controler/SubscribtionController.java
@@ -0,0 +1,37 @@
+package km.hw52.microgram.controler;
+
+import km.hw52.microgram.model.Publication;
+import km.hw52.microgram.model.Subscribtion;
+import km.hw52.microgram.repository.PublicationRepository;
+import km.hw52.microgram.repository.SubscribtionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+
+@org.springframework.stereotype.Controller
+public class SubscribtionController {
+    @Autowired
+    PublicationRepository publicationRepo;
+
+    @Autowired
+    SubscribtionRepository subscribtionRepo;
+
+    @GetMapping("/uifollow/{userId}")
+    public String getPublicationsByUserId(@PathVariable("userId") String userId, Model model) {
+        List<Subscribtion> subs = subscribtionRepo.findAllBySubscriber_Id(userId);
+        List<Publication> publ = new ArrayList<>();
+        for(Subscribtion s : subs) {
+            publ = StreamSupport.stream(publicationRepo.findAllByUserId(s.getSubscribiant().getId()).spliterator(), true )
+                    .collect(Collectors.toList());
+        }
+        model.addAttribute("publications", publ);
+        return "uifollow";
+    }
+}
--- src/main/java/km/hw52/microgram/repository/SubscribtionRepository.java
+++ src/main/java/km/hw52/microgram/repository/SubscribtionRepository.java
@@ -3,7 +3,10 @@ package km.hw52.microgram.repository;
 import km.hw52.microgram.model.Subscribtion;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SubscribtionRepository extends CrudRepository<Subscribtion, String> {
+    List<Subscribtion> findAllBySubscriber_Id(String id);
 }
 // get subscriber
 // get subscribiant
