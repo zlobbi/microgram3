244c63cb516ff985f8263b367df273a1ccbff58a
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-03-18 21:41:43 +0600

    first commit

--- src/main/java/km/hw52/microgram/controler/Controller.java
+++ src/main/java/km/hw52/microgram/controler/Controller.java
@@ -1,13 +1,19 @@
 package km.hw52.microgram.controler;
 
+import km.hw52.microgram.model.User;
+import km.hw52.microgram.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
+    @Autowired
+    UserRepository userRepo;
 
     @GetMapping
-    public String rootHandler() {
-        return "index";
+    public Iterable<User> rootHandler() {
+        return userRepo.findAll();
     }
 
 }
